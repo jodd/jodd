ae51a484fc961a0c697c4d6dbd5a52dec9c9d368
Joris Durand <joris.durand@free.fr>
2015-01-29 13:22:46 +0100

    add a favicon

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
 
 var app = express();
 
@@ -7,6 +8,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
