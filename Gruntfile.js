f9df019a527c28be7dfb808a8e827f0036bd071c
Joris Durand <joris.durand@free.fr>
2015-02-05 10:42:50 +0100

    update gruntfile adding tasks (cssmin, uglify, imagemin, etc.)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,98 @@
-/*global module:false*/
 module.exports = function(grunt) {
 
-  // Project configuration.
   grunt.initConfig({
-    // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
-    // Task configuration.
+    // clean: ['build/*'],
+    copy: {
+      main: {
+        files: [{
+          src: 'public/bower_components/normalize.css/normalize.css',
+          dest: 'public/scss/_normalize.scss'
+        }, {
+          src: 'public/bower_components/modernizr/modernizr.js',
+          dest: 'public/js/lib/modernizr.js'
+        }, {
+          src: 'public/bower_components/requirejs/require.js',
+          dest: 'public/js/lib/require.js'
+        }]
+      }
+    },
     sass: {
-      dist: {
-        files: {
-          'public/css/main.css': 'public/scss/main.scss'
+      main: {
+        files: [{
+          src: 'public/scss/main.scss',
+          dest: 'public/css/main.css'
+        }]
+      }
+    },
+    cssmin: {
+      main: {
+        src: 'public/css/main.css',
+        dest: 'public/css/main.css'
+      }
+    },
+    imagemin: {
+      img: {
+        files: [{
+          expand: true,
+          cwd: 'public/img/src',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'public/img'
+        }]
+      },
+      css: {
+        files: [{
+          expand: true,
+          cwd: 'public/css/img/src',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'public/css/img'
+        }]
+      }
+    },
+    jshint: {
+      gruntfile: ['gruntfile.js'],
+      main: {
+        src: ['public/js/*.js'],
+        options: {
+          globals: {
+            jQuery: true,
+            console: true,
+            module: true
+          }
         }
       }
     },
+    uglify: {
+      lib: {
+        files: [{
+          expand: true,
+          cwd: 'public/js/lib',
+          src: ['**/*.js'],
+          dest: 'public/js/lib'
+        }]
+      }
+    },
     watch: {
+      jshint: {
+        files: ['<%= jshint.files %>'],
+        tasks: ['jshint']
+      },
       sass: {
-        files: ['public/scss/*.scss'],
+        files: ['<%= sass.main.files %>'],
         tasks: ['sass']
       }
-      // lib_test: {
-      //   files: '<%= jshint.lib_test.src %>',
-      //   tasks: ['jshint:lib_test', 'qunit']
-      // }
     }
   });
 
-  // These plugins provide necessary tasks.
+  // grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-  // Default task.
-  grunt.registerTask('default', ['sass']);
-
+  // grunt.registerTask('build', ['clean', 'copy', 'cssmin', 'uglify', 'requirejs']);
+  grunt.registerTask('default', ['copy', 'sass']);
+  grunt.registerTask('build', ['copy', 'cssmin', 'uglify', 'imagemin']);
 };
